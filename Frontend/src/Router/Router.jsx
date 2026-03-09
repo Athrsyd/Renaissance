38ec3341247c89e34e0f4ff8111b1811e25caa71
Athrysd <221656567+Athrysd@users.noreply.github.com>
2026-03-09 20:21:15 +0700

    modified:   Frontend/src/Router/Router.jsx 	new file:   Frontend/src/components/LandingPage2.jsx

--- Frontend/src/Router/Router.jsx
+++ Frontend/src/Router/Router.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import LandingPage from '../pages/LandingPage'
 import { Route, Routes} from 'react-router-dom'
+import LandingPage2 from '../components/LandingPage2'
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/landing2" element={<LandingPage2 />} />
     </Routes>
   )
 }
---
+++ Frontend/src/components/LandingPage2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingPage2 = () => {
+  return (
+    <div>LandingPage2
+        hello
+    </div>
+  )
+}
+
+export default LandingPage2
