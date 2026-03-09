1bdb18d4986537e9ea35cfe1d58089df40e7489f
lutfiidhampuro <lutfiidhampuro@gmail.com>
2026-03-09 08:06:27 +0700

    new file

---
+++ Frontend/src/components/navbar.jsx
@@ -0,0 +1,59 @@
+import logo from '../assets/logo2.png'
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <header className=" fixed w-full flex justify-center unded-full z-50">
+      <nav className=" bg-bistre z-order-last flex p-3 mt-4 justify-center items-center gap-20 w-full max-w-6xl rounded-full">
+        <a
+          href="#about"
+          className="text-beige hover:text-chamoisee transition-colors text-lg"
+        >
+          About Us
+        </a>
+        <a
+          href="#academy"
+          className="text-beige hover:text-chamoisee transition-colors text-lg"
+        >
+          The Academy
+        </a>
+
+        <div className="logo">
+          <a href="/" className="flex items-center p-2 gap-2">
+            <img src={logo} alt="Renaissance Logo" className="h-7" />
+            <h1 className="text-white text-2xl font-semibold">Renaissance</h1>
+          </a>
+        </div>
+
+        <a
+          href="#features"
+          className="text-beige hover:text-chamoisee transition-colors text-lg"
+        >
+          Features
+        </a>
+        <a
+          href="#reviews"
+          className="text-beige hover:text-chamoisee transition-colors text-lg"
+        >
+          Reviews
+        </a>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar
---
+++ Frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Navbar from "../components/navbar";
+import buku from "../assets/buku.png";
+
+const LandingPage = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="h-screen w-full pt-32 flex px-32 flex-col justify-center items-center bg-[#F2E0D2]">
+        <div className="flex">
+          <h1 className="text-6xl font-semibold text-center text-bistre mb-6">
+            <span className="text-chamoisee">Transforming</span> The Way <br />{" "}
+            Students Learn For The Future
+          </h1>
+        </div>
+        <div className="flex flex-row w-full h-screen justify-between items-center gap-10">
+          <div className="w-[30%] pl-7 mb-5 leading-loose">
+            <p className="text-md text-bistre">
+              Modern learning platform designed to help middle and high school
+              students understand lessons better, learn faster, and achieve
+              their academic goals.
+            </p>
+          </div>
+          <div className="flex justify-center items-center w-[40%] h-full">
+            <img width="100%" src={buku} alt="gambar buku" className="w-full" />
+          </div>
+          <div className="flex flex-col justify-center">
+            <div className="w-[30%] text-center mb-5 pr-7 leading-loose">
+              <p className="text-md text-bistre">
+                <span className="text-2xl font-bold">6+ Subject</span>
+                <br />
+                For Middle and High School
+                <br />
+              </p>
+            </div>
+            <a>
+              <button className="bg-chamoisee text-beige px-6 py-3 rounded-full hover:bg-coffee transition-colors">
+                Get Started
+              </button>
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default LandingPage;
