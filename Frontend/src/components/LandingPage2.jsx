dc6c8c3d3a00676cbb1896c495ea53a04a2e7976
NixonValentino <Nicocoyo02@gmail.com>
2026-03-10 16:05:16 +0700

    Tambahan infinite slide, nambah css di index, udh sih itu aja

--- Frontend/src/components/LandingPage2.jsx
+++ Frontend/src/components/LandingPage2.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+
+// Assets Icon
 import iconBintang from "../assets/icon/iconBintang.png";
 import iconKertas from "../assets/icon/iconKertas.png";
 import iconComunity from "../assets/icon/iconComunity.png";
 import iconChart from "../assets/icon/iconChart.png";
 import iconAi from "../assets/icon/iconAi.png";
 import bintangs from "../assets/icon/bintangs.png";
+
+// Assets Profile What They Say
 import nares from "../assets/displayProfile/nares.jpg";
+import william from "../assets/displayProfile/2_william.jpg";
+import cinta from "../assets/displayProfile/3_cinta.jpg";
+import genta from "../assets/displayProfile/4_genta.jpg";
+import arsi from "../assets/displayProfile/5_arsi.jpg";
+import mahendra from "../assets/displayProfile/6_mahendra.jpg";
 
 const LandingPage2 = () => {
   return (
@@ -109,98 +118,187 @@ const LandingPage2 = () => {
           <h1>What Students Say</h1>
         </div>
 
-        <div className="font-poppins mt-15 flex gap-7 overflow-x-hidden">
-          <div className="bg-white w-140 h-70 p-4 rounded-tr-3xl rounded-br-3xl">
-            <div className="flex gap-5 items-center">
-              <img src={nares} alt="" className="w-15 h-15 rounded-full" />
-              <div>
-                <h1 className="text-black font-semibold text-lg">Naresha Letrakusuma</h1>
-                <p className="-mt-1 text-base font-">Grade 12</p>
+        <div>
+          {/* kotak container 1 */}
+          <div className="font-poppins mt-15 flex gap-7 overflow-x-hidden w-max animate-slide">
+            {/* card 1 */}
+            <div className="bg-white w-140 h-70 p-4 rounded-tr-3xl rounded-br-3xl">
+              <div className="flex gap-5 items-center">
+                <img src={nares} alt="" className="w-15 h-15 rounded-full" />
+                <div>
+                  <h1 className="text-black font-semibold text-lg">
+                    Naresha Letrakusuma
+                  </h1>
+                  <p className="-mt-1 text-base font-">Grade 12</p>
+                </div>
+                <p className="-mt-4.5 -mr-2.5 font-extralight">3w Ago</p>
               </div>
-              <p className="-mt-4.5 -mr-2.5 font-extralight">3w Ago</p>
-            </div>
 
-            <div className="mt-3 font-semibold">
-              <p>
-                Belajar di Renaissance terasa jauh lebih menyenangkan
-                dibandingkan platform belajar lain yang pernah aku coba.
-                Materinya dijelaskan dengan jelas dan tampilannya juga nyaman
-                dilihat, jadi aku bisa belajar lebih lama tanpa merasa bosan
-              </p>
-              <img src={bintangs} alt="" className="-ml-3"/>
-            </div>
-          </div>
-
-          <div className="bg-white w-145 h-70 p-4 rounded-tr-3xl rounded-3xl ">
-            <div className="flex gap-5 items-center">
-              <img src={nares} alt="" className="w-15 h-15 rounded-full" />
-              <div>
-                <h1 className="text-black font-semibold text-lg">Naresha Letrakusuma</h1>
-                <p className="-mt-1 text-base font-">Grade 12</p>
+              <div className="mt-3 font-semibold">
+                <p>
+                  Belajar di Renaissance terasa jauh lebih menyenangkan
+                  dibandingkan platform belajar lain yang pernah aku coba.
+                  Materinya dijelaskan dengan jelas dan tampilannya juga nyaman
+                  dilihat, jadi aku bisa belajar lebih lama tanpa merasa bosan
+                </p>
+                <img src={bintangs} alt="" className="-ml-3" />
               </div>
-              <p className="-mt-4.5 -mr-2.5 font-extralight">3w Ago</p>
             </div>
 
-            <div className="mt-3 font-semibold">
-              <p>
-                Belajar di Renaissance terasa jauh lebih menyenangkan
-                dibandingkan platform belajar lain yang pernah aku coba.
-                Materinya dijelaskan dengan jelas dan tampilannya juga nyaman
-                dilihat, jadi aku bisa belajar lebih lama tanpa merasa bosan
-              </p>
-              <img src={bintangs} alt="" className="-ml-3"/>
-            </div>
-          </div>
-
-          <div className="bg-white w-130 h-70 p-4 rounded-tr-3xl rounded-3xl -mr-20">
-            <div className="flex gap-5 items-center">
-              <img src={nares} alt="" className="w-15 h-15 rounded-full" />
-              <div>
-                <h1 className="text-black font-semibold text-lg">Naresha Letrakusuma</h1>
-                <p className="-mt-1 text-base font-">Grade 12</p>
+            {/* card 2 */}
+            <div className="bg-white w-145 h-70 p-4 rounded-tr-3xl rounded-3xl ">
+              <div className="flex gap-5 items-center">
+                <img src={william} alt="" className="w-15 h-15 rounded-full" />
+                <div>
+                  <h1 className="text-black font-semibold text-lg">
+                    William Jakraputra
+                  </h1>
+                  <p className="-mt-1 text-base font-">Grade 10</p>
+                </div>
+                <p className="-mt-4.5 -mr-2.5 font-extralight">3w Ago</p>
               </div>
-              <p className="-mt-4.5 -mr-2.5 font-extralight">3w Ago</p>
+
+              <div className="mt-3 font-semibold">
+                <p>
+                  Fitur AI di Renaissance sangat membantu ketika aku kesulitan
+                  memahami soal. Penjelasannya cepat dan bahasanya mudah
+                  dimengerti, jadi aku bisa langsung paham tanpa harus mencari
+                  jawaban di tempat lain.
+                </p>
+                <img src={bintangs} alt="" className="-ml-3" />
+              </div>
             </div>
 
-            <div className="mt-3 font-semibold">
-              <p>
-                Belajar di Renaissance terasa jauh lebih menyenangkan
-                dibandingkan platform belajar lain yang pernah aku coba.
-                Materinya dijelaskan dengan jelas dan tampilannya juga nyaman
-                dilihat, jadi aku bisa belajar lebih lama tanpa merasa bosan
-              </p>
-              <img src={bintangs} alt="" className="-ml-3"/>
+            {/* card 3 */}
+            <div className="bg-white w-130 h-70 p-4 rounded-tr-3xl rounded-3xl ">
+              <div className="flex gap-5 items-center">
+                <img src={cinta} alt="" className="w-15 h-15 rounded-full" />
+                <div>
+                  <h1 className="text-black font-semibold text-lg">
+                    Cinta Reynasya
+                  </h1>
+                  <p className="-mt-1 text-base font-">Grade 12</p>
+                </div>
+                <p className="-mt-4.5 -mr-2.5 font-extralight">1w Ago</p>
+              </div>
+
+              <div className="mt-3 font-semibold">
+                <p>
+                  Yang paling aku suka adalah fitur komunitasnya. Aku bisa
+                  berdiskusi dengan siswa lain, bertanya kalau ada materi yang
+                  sulit, dan kadang juga membantu menjelaskan ke teman lain
+                </p>
+                <img src={bintangs} alt="" className="-ml-3" />
+              </div>
+            </div>
+
+            {/* Duplicate */}
+            {/* card 1 */}
+            <div className="bg-white w-140 h-70 p-4 rounded-3xl">
+              <div className="flex gap-5 items-center">
+                <img src={nares} alt="" className="w-15 h-15 rounded-full" />
+                <div>
+                  <h1 className="text-black font-semibold text-lg">
+                    Naresha Letrakusuma
+                  </h1>
+                  <p className="-mt-1 text-base font-">Grade 12</p>
+                </div>
+                <p className="-mt-4.5 -mr-2.5 font-extralight">3w Ago</p>
+              </div>
+
+              <div className="mt-3 font-semibold">
+                <p>
+                  Belajar di Renaissance terasa jauh lebih menyenangkan
+                  dibandingkan platform belajar lain yang pernah aku coba.
+                  Materinya dijelaskan dengan jelas dan tampilannya juga nyaman
+                  dilihat, jadi aku bisa belajar lebih lama tanpa merasa bosan
+                </p>
+                <img src={bintangs} alt="" className="-ml-3" />
+              </div>
+            </div>
+
+            {/* card 2 */}
+            <div className="bg-white w-145 h-70 p-4 rounded-tr-3xl rounded-3xl ">
+              <div className="flex gap-5 items-center">
+                <img src={william} alt="" className="w-15 h-15 rounded-full" />
+                <div>
+                  <h1 className="text-black font-semibold text-lg">
+                    William Jakraputra
+                  </h1>
+                  <p className="-mt-1 text-base font-">Grade 10</p>
+                </div>
+                <p className="-mt-4.5 -mr-2.5 font-extralight">3w Ago</p>
+              </div>
+
+              <div className="mt-3 font-semibold">
+                <p>
+                  Fitur AI di Renaissance sangat membantu ketika aku kesulitan
+                  memahami soal. Penjelasannya cepat dan bahasanya mudah
+                  dimengerti, jadi aku bisa langsung paham tanpa harus mencari
+                  jawaban di tempat lain.
+                </p>
+                <img src={bintangs} alt="" className="-ml-3" />
+              </div>
+            </div>
+
+            {/* card 3 */}
+            <div className="bg-white w-130 h-70 p-4 rounded-tr-3xl rounded-3xl -mr-20">
+              <div className="flex gap-5 items-center">
+                <img src={cinta} alt="" className="w-15 h-15 rounded-full" />
+                <div>
+                  <h1 className="text-black font-semibold text-lg">
+                    Cinta Reynasya
+                  </h1>
+                  <p className="-mt-1 text-base font-">Grade 12</p>
+                </div>
+                <p className="-mt-4.5 -mr-2.5 font-extralight">3w Ago</p>
+              </div>
+
+              <div className="mt-3 font-semibold">
+                <p>
+                  Yang paling aku suka adalah fitur komunitasnya. Aku bisa
+                  berdiskusi dengan siswa lain, bertanya kalau ada materi yang
+                  sulit, dan kadang juga membantu menjelaskan ke teman lain
+                </p>
+                <img src={bintangs} alt="" className="-ml-3" />
+              </div>
             </div>
           </div>
         </div>
 
-        <div className="font-poppins mt-5 flex gap-7 overflow-x-hidden ">
-          <div className="bg-white w-140 h-70 p-4 rounded-tr-3xl rounded-br-3xl -ml-20">
+        {/* kotak container 2 */}
+        <div className="font-poppins mt-5 flex gap-7 overflow-x-hidden w-max animate-slide-reverse">
+          {/* card 4 */}
+          <div className="bg-white w-140 h-70 p-4 rounded-3xl -ml-20 ">
             <div className="flex gap-5 items-center">
-              <img src={nares} alt="" className="w-15 h-15 rounded-full" />
+              <img src={genta} alt="" className="w-15 h-15 rounded-full" />
               <div>
-                <h1 className="text-black font-semibold text-lg">Naresha Letrakusuma</h1>
-                <p className="-mt-1 text-base font-">Grade 12</p>
+                <h1 className="text-black font-semibold text-lg">
+                  Genta Narendra
+                </h1>
+                <p className="-mt-1 text-base font-">Grade 9</p>
               </div>
-              <p className="-mt-4.5 -mr-2.5 font-extralight">3w Ago</p>
+              <p className="-mt-4.5 -mr-2.5 font-extralight">2w Ago</p>
             </div>
 
             <div className="mt-3 font-semibold">
               <p>
-                Belajar di Renaissance terasa jauh lebih menyenangkan
-                dibandingkan platform belajar lain yang pernah aku coba.
-                Materinya dijelaskan dengan jelas dan tampilannya juga nyaman
-                dilihat, jadi aku bisa belajar lebih lama tanpa merasa bosan
+                Pembelajarannya interaktif dan tidak membosankan. Ada banyak
+                cara untuk memahami materi, jadi rasanya seperti belajar sambil
+                eksplorasi, bukan hanya membaca teks saja
               </p>
-              <img src={bintangs} alt="" className="-ml-3"/>
+              <img src={bintangs} alt="" className="-ml-3" />
             </div>
           </div>
 
+          {/* card 5 */}
           <div className="bg-white w-150 h-70 p-4 rounded-tr-3xl rounded-3xl">
             <div className="flex gap-5 items-center">
-              <img src={nares} alt="" className="w-15 h-15 rounded-full" />
+              <img src={mahendra} alt="" className="w-15 h-15 rounded-full" />
               <div>
-                <h1 className="text-black font-semibold text-lg">Naresha Letrakusuma</h1>
+                <h1 className="text-black font-semibold text-lg">
+                  Mahendra Pratama
+                </h1>
                 <p className="-mt-1 text-base font-">Grade 12</p>
               </div>
               <p className="-mt-4.5 -mr-2.5 font-extralight">3w Ago</p>
@@ -208,23 +306,50 @@ const LandingPage2 = () => {
 
             <div className="mt-3 font-semibold">
               <p>
-                Belajar di Renaissance terasa jauh lebih menyenangkan
-                dibandingkan platform belajar lain yang pernah aku coba.
-                Materinya dijelaskan dengan jelas dan tampilannya juga nyaman
-                dilihat, jadi aku bisa belajar lebih lama tanpa merasa bosan
+                Renaissance membuat belajar terasa lebih terstruktur. Aku bisa
+                melihat materi dengan jelas dan belajar secara bertahap tanpa
+                merasa kewalahan
               </p>
-              <img src={bintangs} alt="" className="-ml-3"/>
+              <img src={bintangs} alt="" className="-ml-3" />
             </div>
           </div>
 
+          {/* card 6 */}
           <div className="bg-white w-135 h-70 p-4 rounded-tr-3xl rounded-3xl">
             <div className="flex gap-5 items-center">
-              <img src={nares} alt="" className="w-15 h-15 rounded-full" />
+              <img src={arsi} alt="" className="w-15 h-15 rounded-full" />
               <div>
-                <h1 className="text-black font-semibold text-lg">Naresha Letrakusuma</h1>
+                <h1 className="text-black font-semibold text-lg">
+                  Arsi Kealan
+                </h1>
+                <p className="-mt-1 text-base font-">Grade 7</p>
+              </div>
+              <p className="-mt-4.5 -mr-2.5 font-extralight">1w Ago</p>
+            </div>
+
+            <div className="mt-3 font-semibold">
+              <p>
+                Awalnya aku mencoba Renaissance karena direkomendasikan oleh
+                teman. Setelah mencobanya, aku merasa platform ini benar-benar
+                membantu memahami beberapa pelajaran yang sebelumnya terasa
+                sulit
+              </p>
+              <img src={bintangs} alt="" className="-ml-3" />
+            </div>
+          </div>
+
+          {/* Duplicate */}
+          {/* card 4 */}
+          <div className="bg-white w-140 h-70 p-4 rounded-3xl">
+            <div className="flex gap-5 items-center">
+              <img src={genta} alt="" className="w-15 h-15 rounded-full" />
+              <div>
+                <h1 className="text-black font-semibold text-lg">
+                   Genta Narendra
+                </h1>
                 <p className="-mt-1 text-base font-">Grade 12</p>
               </div>
-              <p className="-mt-4.5 -mr-2.5 font-extralight">3w Ago</p>
+              <p className="-mt-4.5 -mr-2.5 font-extralight">2w Ago</p>
             </div>
 
             <div className="mt-3 font-semibold">
@@ -234,7 +359,53 @@ const LandingPage2 = () => {
                 Materinya dijelaskan dengan jelas dan tampilannya juga nyaman
                 dilihat, jadi aku bisa belajar lebih lama tanpa merasa bosan
               </p>
-              <img src={bintangs} alt="" className="-ml-3"/>
+              <img src={bintangs} alt="" className="-ml-3" />
+            </div>
+          </div>
+
+          {/* card 5 */}
+          <div className="bg-white w-145 h-70 p-4 rounded-tr-3xl rounded-3xl ">
+            <div className="flex gap-5 items-center">
+              <img src={mahendra} alt="" className="w-15 h-15 rounded-full" />
+              <div>
+                <h1 className="text-black font-semibold text-lg">
+                  Mahendra Pratama
+                </h1>
+                <p className="-mt-1 text-base font-">Grade 12</p>
+              </div>
+              <p className="-mt-4.5 -mr-2.5 font-extralight">3w Ago</p>
+            </div>
+
+            <div className="mt-3 font-semibold">
+              <p>
+                Renaissance membuat belajar terasa lebih terstruktur. Aku bisa
+                melihat materi dengan jelas dan belajar secara bertahap tanpa
+                merasa kewalahan
+              </p>
+              <img src={bintangs} alt="" className="-ml-3" />
+            </div>
+          </div>
+
+          {/* card 6 */}
+          <div className="bg-white w-130 h-70 p-4 rounded-tr-3xl rounded-3xl -mr-20">
+            <div className="flex gap-5 items-center">
+              <img src={arsi} alt="" className="w-15 h-15 rounded-full" />
+              <div>
+                <h1 className="text-black font-semibold text-lg">
+                  Arsi Kealan
+                </h1>
+                <p className="-mt-1 text-base font-">Grade 7</p>
+              </div>
+              <p className="-mt-4.5 -mr-2.5 font-extralight">3w Ago</p>
+            </div>
+
+            <div className="mt-3 font-semibold">
+              <p>
+                Awalnya aku mencoba Renaissance karena direkomendasikan oleh
+                teman. Setelah mencobanya, aku merasa platform ini benar-benar
+                membantu memahami beberapa pelajaran yang sebelumnya terasa sulit
+              </p>
+              <img src={bintangs} alt="" className="-ml-3" />
             </div>
           </div>
         </div>
