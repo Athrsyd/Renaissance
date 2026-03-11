4748a0b643b914f5ee9329ddda26de78b5f95802
Athrysd <221656567+Athrysd@users.noreply.github.com>
2026-03-11 13:25:31 +0700

    modified:   Frontend/eslint.config.js 	modified:   Frontend/package-lock.json 	modified:   Frontend/package.json 	modified:   Frontend/src/Hook/HookAuth.jsx 	modified:   Frontend/src/pages/AuthPages/Login.jsx 	modified:   Frontend/src/pages/AuthPages/Register.jsx 	new file:   Frontend/src/services/api.js

--- Frontend/src/Hook/HookAuth.jsx
+++ Frontend/src/Hook/HookAuth.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
-
+// import React from 'react'
+import { useState } from 'react'
+import API from '../services/api'
+import { useNavigate } from 'react-router-dom'
+// import axios from 'axios'
+// 
 const HookAuth = () => {
-  return (
-    <div>HookAuth</div>
-  )
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmitRegister = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await API.post('/auth/register', { email, name, password });
+      console.log(response.data);
+      setEmail('');
+      setName('');
+      setPassword('');
+      setMessage(response.data.message);
+    } catch (error) {
+      console.error('Error registering user:', error);
+      setMessage(error.response.data.message);
+    }
+  };
+
+  const handleSubmitLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await API.post('/auth/login', { email, password });
+      console.log(response.data);
+      setEmail('');
+      setMessage(response.data.message);
+      setPassword('');
+      setTimeout(() => {
+        navigate('/landing2');
+      }, 1000);
+    } catch (error) {
+      console.error('Error logging in user:', error);
+      setMessage(error.response.data.message);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'name') setName(value);
+    else if (name === 'email') setEmail(value);  
+    else if (name === 'password') setPassword(value);
+  }
+
+  return {
+    email,
+    name,
+    password,
+    message,
+    handleChange,
+    handleSubmitRegister,
+    handleSubmitLogin
+  }
+
 }
 
 export default HookAuth
