6780d0525f6d75fac0e8983d83fb0f8b6a18fa14
lutfiidhampuro <lutfiidhampuro@gmail.com>
2026-03-11 13:25:10 +0700

    new file:   Frontend/src/components/Counter.jsx 	modified:   Frontend/src/components/Mapel.jsx 	modified:   Frontend/src/components/Navbar.jsx 	modified:   Frontend/src/components/Testimoni.jsx 	modified:   Frontend/src/pages/LandingPage.jsx

--- Frontend/src/components/Mapel.jsx
+++ Frontend/src/components/Mapel.jsx
@@ -1,18 +1,69 @@
 
-const Mapel = ({ namaMapel, deskripsi }) => {
+const dataMapel = [
+  {
+    id: 1,
+    namaMapel: "Matematika",
+    deskripsi:
+      "Ilmu yang mempelajari angka, pola, dan logika untuk memahami cara kerja berbagai fenomena di dunia.",
+  },
+  {
+    id: 2,
+    namaMapel: "IPA",
+    deskripsi:
+      "Mempelajari alam semesta melalui pengamatan dan eksperimen, mulai dari fisika, kimia, hingga biologi.",
+  },
+  {
+    id: 3,
+    namaMapel: "IPS",
+    deskripsi:
+      "Ilmu yang membahas kehidupan masyarakat, ekonomi, geografi, dan hubungan antar manusia dalam dunia sosial.",
+  },
+  {
+    id: 4,
+    namaMapel: "Sejarah",
+    deskripsi:
+      "Mempelajari peristiwa masa lalu untuk memahami perkembangan peradaban manusia dan dampaknya pada masa kini.",
+  },
+  {
+    id: 5,
+    namaMapel: "Bahasa dan Sastra",
+    deskripsi:
+      "Mengembangkan kemampuan berbahasa, memahami karya sastra, serta mengekspresikan ide dan perasaan melalui tulisan.",
+  },
+  {
+    id: 6,
+    namaMapel: "PKN",
+    deskripsi:
+      "Memahami nilai-nilai kebangsaan, hukum, serta peran setiap individu dalam membangun masyarakat yang adil dan harmonis.",
+  },
+];
+
+const MapelList = ({ item }) => {
   return (
     <>
       <div className="flex flex-row w-[60%] justify-between text-center">
         <div className="bg-[#9B7A5B] w-70 h-20 p-6 rounded-2xl">
-          <h2 className="text-xl font-semibold text-white">{namaMapel}</h2>
+          <h2 className="text-xl font-semibold text-white">{item.namaMapel}</h2>
         </div>
         <div className="relative w-0.5 h-35 bg-[#9B7A5B]"></div>
         <div className="w-70">
-          <p className="text-md font-medium text-justify text-white">{deskripsi}</p>
+          <p className="text-md font-medium text-justify text-white">{item.deskripsi}</p>
         </div>
       </div>
     </>
   );
 };
+
+const Mapel = () =>{
+  return (
+    <>
+      <div className="flex flex-col w-full items-center justify-center">
+        {dataMapel.map((item) => (
+          <MapelList key={item.id} item={item} />
+        ))}
+      </div>
+    </>
+  );
+}
 
 export default Mapel
