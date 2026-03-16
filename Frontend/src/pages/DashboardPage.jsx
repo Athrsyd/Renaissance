f44789d9633d057f0eb9a82b998ddc8a0e827b34
lutfiidhampuro <lutfiidhampuro@gmail.com>
2026-03-17 01:31:26 +0700

    new file:   Frontend/src/assets/icon/notifIcon.svg 	new file:   Frontend/src/assets/icon/rowBottom.svg 	new file:   Frontend/src/assets/icon/searchIcon.svg 	modified:   Frontend/src/pages/DashboardPage.jsx

--- Frontend/src/pages/DashboardPage.jsx
+++ Frontend/src/pages/DashboardPage.jsx
@@ -1,27 +1,36 @@
 import React from 'react'
 import NavDasboard from "../components/NavDasboard"
+import Logo from "../assets/Logo2.png"
+import Search from "../assets/icon/searchIcon.svg"
+import Notif from "../assets/icon/notifIcon.svg"
+import CominityList from "../components/CommunityList"
+import rBottom from "../assets/icon/rowBottom.svg"
 
 const DashboardPage = () => {
   return (
     <>
-      <div className="flex flex-col bg-white justify-center items-center">
+      <div className="flex flex-col bg-white justify-center items-center overflow-x-hidden">
         <NavDasboard />
-        <div className="flex flex-row w-full ml-20 mt-5 justify-center items-center">
+        <div className="flex flex-row w-full ml-20 mt-2 justify-center items-center">
           <div className="relative justify-center items-center">
             <input
               type="search"
-              id="site-search"
-              name="q"
               placeholder="Explore Lessons"
-              className="bg-[#D5D4D4] text-center text-sm rounded-xl w-100 h-10 outline-0"
+              className="bg-[#D5D4D4] z-50 text-center text-sm rounded-xl w-150 h-10 outline-0"
             />
+            <img src={Search} className="w-5 z-99 -mt-7 ml-2" />
           </div>
-          <div className="ml-30">
+          <div className="ml-10 mt-6 gap-5 flex flex-row justify-between">
             <button>
-              <img src="Logo" />
+              <img src={Notif} className="w-7" />
+            </button>
+            <div className="img h-12 w-12 mb-2 bg-bistre rounded-full"></div>
+            <button>
+              <img src={rBottom} className="w-5"/>
             </button>
           </div>
         </div>
+        <CominityList />
       </div>
     </>
   );
