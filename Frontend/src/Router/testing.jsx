831623ddd14b7b3b8eec19acad2b052e67f31af6
Athrysd <221656567+Athrysd@users.noreply.github.com>
2026-03-17 01:17:34 +0700

    modified:   Frontend/src/Router/testing.jsx new file:   Frontend/src/components/CommunityList.jsx

--- Frontend/src/Router/testing.jsx
+++ Frontend/src/Router/testing.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from '../components/navbar'
 import Mapel from '../components/Mapel'
+import CommunityList from '../components/CommunityList'
 
 const Testing = () => {
   return (
     <>
       <Navbar/>
       <Mapel/>
+      <CommunityList/>
+
     </>
   )
 }
---
+++ Frontend/src/components/CommunityList.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const CommunityList = () => {
+    const communities = [
+        {
+            id: 1,
+            name: 'Math Forum',
+            participants: 1290,
+        },
+        {
+            id: 2,
+            name: 'Science Hub',
+            participants: 980,
+        },
+        {
+            id: 3,
+            name: 'Literature Lounge',
+            participants: 750,
+        },
+    ]
+
+    return (
+        <div className="community-list-container p-4  bg-bistre w-225 mx-auto text-beige rounded-2xl mt-6 ">
+            <div className="title text-2xl text-center font-semibold px-25 py-4">
+                <h1 className=''>Ruang diskusi bagi para pelajar untuk bertanya,
+                    berbagi pengetahuan, dan belajar bersama dalam
+                    komunitas Renaissance.</h1>
+            </div>
+            <div className="community-list">
+                {communities.map((community) => (
+
+                    <div className="community-item flex flex-row px-10 justify-between items-center p-4 bg-white text-bistre rounded-lg mt-4">
+                        <div className="kiri flex flex-row items-center">
+
+                            <div className="img h-10 w-10 bg-bistre rounded-full"></div>
+                            <div className="ml-4">
+                                <h2 className="text-xl font-bold">{community.name}</h2>
+                                <p className="text-sm">{community.participants} participants</p>
+                            </div>
+                        </div>
+                        <div className="kanan">
+                            <button className='bg-beige text-bistre hover:bg-bistre hover:text-beige text-md font-semibold rounded-md transition duration-200 ease-in-out py-1 px-8'>join</button>
+                        </div>
+                    </div>))}
+            </div>
+        </div>
+    )
+}
+
+export default CommunityList
