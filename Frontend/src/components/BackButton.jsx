1342ee068f815cf94d043d8d6dc0fef2a1609747
Athrysd <221656567+Athrysd@users.noreply.github.com>
2026-03-25 17:19:51 +0700

    improve fe auth

--- Frontend/src/components/BackButton.jsx
+++ Frontend/src/components/BackButton.jsx
@@ -6,9 +6,9 @@ import { FaHome } from "react-icons/fa";
 
 const BackButton = () => {
     return (
-        <div className='absolute top-7 left-7 z-50'>
+        <div className='absolute top-7 left-7 z-50 '>
             <Link to="/" >
-                <button className="bg-coffe flex justify-center items-center text-center rounded-4xl h-10 w-10 md:w-15 md:h-15 text-3xl p-2 text-white hover:bg-coffee transition-colors">
+                <button className="bg-coffe cursor-pointer flex justify-center items-center text-center rounded-4xl h-10 w-10 md:w-15 md:h-15 text-3xl p-2 text-white hover:bg-coffee transition-colors">
                     <FaHome className=" text-white" /> 
                 </button>
             </Link>
